1ce15713893a18307e11cbf25d845f84d6a6793e
fernando.adratt <fernando.adratt@TANSC165.tansc.tan.com.br>
2023-11-21 17:09:42 -0300

    Criação de registro de pontos

---
+++ src/coursera/RegistroPontosTest.java
@@ -0,0 +1,53 @@
+package coursera;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class RegistroPontosTest {
+
+	@Test
+	void pontosCriarTopico() {
+		Usuario u = new Usuario();
+		u.nome = "Guerra";
+		CalculadoraBonus cb = new CalculadoraBonus();
+		RegistroPontos rp = new RegistroPontos( cb );
+		rp.criarUmTopico( u );
+		assertEquals(u.pontos, 5);
+	}
+	
+	@Test
+	void pontosCriarTopicoVip() {
+		Usuario u = new Usuario();
+		u.nome = "Guerra";
+		u.vip = true;
+		CalculadoraBonus cb = new CalculadoraBonus();
+		RegistroPontos rp = new RegistroPontos( cb );
+		rp.criarUmTopico( u );
+		assertEquals(u.pontos, 25);
+	}
+
+	@Test
+	void pontosCriarTopicoBonusDoDia() {
+		Usuario u = new Usuario();
+		u.nome = "Guerra";
+		CalculadoraBonus cb = new CalculadoraBonus();
+		cb.bonusDoDia = 3;
+		RegistroPontos rp = new RegistroPontos( cb );
+		rp.criarUmTopico( u );
+		assertEquals(u.pontos, 15);
+	}
+	
+	@Test
+	void pontosCriarTopicoVipBonusDoDia() {
+		Usuario u = new Usuario();
+		u.nome = "Guerra";
+		u.vip = true;
+		CalculadoraBonus cb = new CalculadoraBonus();
+		cb.bonusDoDia = 2;
+		RegistroPontos rp = new RegistroPontos( cb );
+		rp.criarUmTopico( u );
+		assertEquals(u.pontos, 50);
+	}
+
+}
